cfdb8ba36c7174b0461e659a1d7ace17232765ef
whiteParachute <sandian028@hotmail.com>
2020-03-27 00:39:47 +0800

    默认更改列表

---
+++ script/script_1.py
@@ -0,0 +1,13 @@
+'''
+
+author: whiteP
+
+modified time: {DATE} {TIME}
+
+'''
+from python.demo_1 import Demo1
+from testing.demo2 import Demo2
+
+Demo2()
+
+Demo1()
---
+++ testing/demo2.py
@@ -0,0 +1,10 @@
+'''
+
+author: whiteP
+
+modified time: 2020-3-27 0:08
+
+'''
+
+class Demo2:
+    pass
