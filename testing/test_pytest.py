c3733bf14c35b0fe2134f9640ce5b59da3125c54
whiteParachute <sandian028@hotmail.com>
2020-03-30 00:19:31 +0800

    pytest demo change

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -9,6 +9,11 @@ from python.calc import Calc
 
 
 class TestCalc:
+    def setup(self):
+        self.calc = Calc()
+
     def test_add(self):
-        calc = Calc()
-        assert calc.add(1, 2) == 3
+        assert self.calc.add(1, 2) == 3
+
+    def test_div(self):
+        assert self.calc.div(1, 2) == 0.5
