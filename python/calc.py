caea65b0534db01fbd49af3fe54bb019d40d4a59
whiteParachute <sandian028@hotmail.com>
2020-03-30 00:24:42 +0800

    pytest demo change

--- python/calc.py
+++ python/calc.py
@@ -8,8 +8,8 @@ modified time: {DATE} {TIME}
 
 
 class Calc:
-    def add(self, a, b):
+    def add(self, a: int, b: int) -> int:
         return a + b
 
-    def div(self, a, b):
+    def div(self, a, b) -> float:
         return a / b
