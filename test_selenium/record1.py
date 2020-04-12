4617c0385bc706626cbdc167fec23517e7e0239c
whiteParachute <sandian028@hotmail.com>
2020-04-12 19:21:45 +0800

    排序文件

--- test_selenium/record1.py
+++ test_selenium/record1.py
@@ -17,11 +17,15 @@ from selenium.webdriver.common.by import By
 
 class Test123():
     def setup_method(self, method):
+        # 打开浏览器debug模式，连接到debug模式端口
         chrome_opts = webdriver.ChromeOptions()
         chrome_opts.debugger_address = "127.0.0.1:9222"
         self.driver = webdriver.Chrome(options=chrome_opts)
         # self.driver = webdriver.Chrome()
+
+        # 打开企业微信页面
         self.driver.get("https://work.weixin.qq.com/")
+        # 隐士等待
         self.driver.implicitly_wait(3)
 
     def teardown_method(self, method):
@@ -32,15 +36,19 @@ class Test123():
         # self.driver.find_element(By.CSS_SELECTOR,
         #                          ".index_service_cnt_item_title").click()
         # time.sleep(15)
+        # 记录cookies
         cookies = self.driver.get_cookies()
         with open("cookies.txt", "w") as f:
             json.dump(cookies, f)
+        # 读取cookies
         with open("cookies.txt", "r") as f:
             cookies: List[Dict] = json.load(f)
         for cookie in cookies:
+            # 忽略expiry
             if "expiry" in cookie.keys():
                 cookie.pop("expiry")
             self.driver.add_cookie(cookie)
+        # 自动登录后进入主页面点击“添加成员”bottom
         self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
         self.driver.find_element(By.CSS_SELECTOR,
                                  '.index_service_cnt_itemWrap').click()
